4b3ae35f450931e17a31336e0a28825fd8657f6b
LeonOAO <kkwan812@gmail.com>
2026-02-24 13:22:49 +0800

    Update sw.js

--- ixlmath/sw.js
+++ ixlmath/sw.js
@@ -7,7 +7,7 @@ if (navigator.userAgent.includes("Firefox")) {
 
 // blocklist by s16 and swium - blocklist by s16 and swium - blocklist by s16 and swium - blocklist by s16 and swium - blocklist by s16 and swium
 
-importScripts("/ixlmath/scram/scramjet.all.js");
+importScripts("./ixlmath/scram/scramjet.all.js");
 
 const { ScramjetServiceWorker } = $scramjetLoadWorker();
 const scramjet = new ScramjetServiceWorker();
